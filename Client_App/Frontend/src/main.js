7cb85b3c6a3697c6c1ea89c034c7ae65f64e84f2
Anayybot15 <itzelbotello15@gmail.com>
2024-11-28 09:43:58 -0600

    componentes navbar y competencias

--- Client_App/Frontend/src/main.js
+++ Client_App/Frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Importa los estilos de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importa el JavaScript de Bootstrap (opcional, para componentes interactivos como modales, tooltips, etc.)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+createApp(App).mount('#app');
+
