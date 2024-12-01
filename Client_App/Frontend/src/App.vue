5ced5f92787e38031af9ae1fb5be296e21900a72
Anayybot15 <itzelbotello15@gmail.com>
2024-12-01 12:43:16 -0600

    instalacion de vue router

--- Client_App/Frontend/src/App.vue
+++ Client_App/Frontend/src/App.vue
@@ -1,22 +1,19 @@
-<script setup>
-import Formulario from './components/Formulario.vue';
-</script>
-
 <template>
-  <h1>Hello world</h1>
+  <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+export default {
+  name: 'App',
+  
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
+
--- Client_App/Frontend/src/main.js
+++ Client_App/Frontend/src/main.js
@@ -1,12 +1,12 @@
-// main.js
+
 import { createApp } from 'vue';
 import App from './App.vue';
-
-// Importa los estilos de Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Importa el JavaScript de Bootstrap (opcional, para componentes interactivos como modales, tooltips, etc.)
+import router from './router';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
+
 
